7c386d6d8341f8e15b89f539f85d12b4d1ce7393
alphaeps <alphaeps@Mac.local>
2024-12-29 15:12:44 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,460 @@
+--[[
+	1. vim.o 用于设置和获取全局选项，与编辑器行为或界面相关。
+	2. vim.g 用于设置和获取全局变量，通常与插件或自定义配置相关。
+	3. 如果是窗口或缓冲区相关的选项，则应该分别使用 vim.wo 和 vim.bo，以免混淆。
+--]]
+
+local set = vim.o
+-- 设置编码
+set.fileencoding = "UTF-8"
+vim.g.encoding = "UTF-8"
+-- 开启行号
+set.number = true
+-- 开启相对行号
+set.relativenumber = true
+-- 复制到剪切板
+vim.opt.clipboard = "unnamedplus"
+-- set.clipboard = "unnamed"
+-- 在 copy 后高亮
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
+})
+-- keybindings 快捷键绑定
+local opt = { noremap = true, silent = true }
+vim.g.mapleader = " "
+vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
+vim.keymap.set(
+	"n",
+	"<Leader>v",
+	"<C-w>v",
+	vim.tbl_extend("force", opt, { desc = "<Leader>v -> <C-w>v: 左右复制窗口" })
+)
+vim.keymap.set(
+	"n",
+	"<Leader>s",
+	"<C-w>s",
+	vim.tbl_extend("force", opt, { desc = "<Leader>s -> <C-w>s: 上下复制窗口" })
+)
+-- 在可视行中使用 jk 移动，参考：https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
+
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{
+		"camspiers/luarocks",
+		dependencies = {
+			"rcarriga/nvim-notify", -- Optional dependency
+		},
+		opts = {
+			rocks = { "fzy" }, -- Specify LuaRocks packages to install
+		},
+	},
+	{
+		"RRethy/nvim-base16",
+		lazy = true,
+	},
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		opts = {
+			-- add any custom options here
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{ "echasnovski/mini.nvim", version = "*" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		-- or                              , branch = '0.1.x',
+		dependencies = { "nvim-lua/plenary.nvim" },
+		cmd = "Telescope",
+		keys = {
+			{ "<leader>P", ":Telescope find_files<CR>", desc = "搜索文件" },
+			{ "<leader>p", ":Telescope live_grep<CR>", desc = "搜索文件内容" },
+			{ "<leader>rs", ":Telescope resume<CR>", desc = "恢复搜索" },
+			{ "<leader>q", ":Telescope oldfiles<CR>", desc = "列出之前打开的文件" },
+		},
+	},
+	{
+		event = "VeryLazy",
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+	},
+	{
+		event = "VeryLazy",
+		"neovim/nvim-lspconfig",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+	},
+	{
+		event = "VeryLazy",
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+		},
+	},
+	{
+		"folke/neodev.nvim",
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+	{
+		event = "VeryLazy",
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.goimports,
+				},
+				on_attach = function(client, bufnr)
+					vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+						border = "rounded", -- 圆角边框
+					})
+
+					vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+						border = "rounded",
+					})
+
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+								vim.lsp.buf.format({ bufnr = bufnr })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"onsails/lspkind.nvim", -- 提供补全项类型的图标
+	},
+	{
+		event = "VeryLazy",
+		"tpope/vim-fugitive",
+		cmd = "Git",
+		config = function()
+			-- convert
+			vim.cmd.cnoreabbrev([[git Git]])
+			vim.cmd.cnoreabbrev([[gp Git push]])
+		end,
+	},
+	{
+		event = "VeryLazy",
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+})
+-- 启用主题
+vim.cmd.colorscheme("base16-ocean")
+
+-- LSP 设置
+local lspconfig = require("lspconfig")
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+		-- Buffer
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<leader>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<leader>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, opts)
+	end,
+})
+
+require("mason").setup({
+	install_root_dir = vim.fn.stdpath("data") .. "/mason",
+	PATH = "prepend",
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
+	registries = {
+		"github:mason-org/mason-registry",
+	},
+	providers = {
+		"mason.providers.registry-api",
+		"mason.providers.client",
+	},
+	github = {
+		---@since 1.0.0
+		-- The template URL to use when downloading assets from GitHub.
+		-- The placeholders are the following (in order):
+		-- 1. The repository (e.g. "rust-lang/rust-analyzer")
+		-- 2. The release version (e.g. "v0.3.0")
+		-- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
+		download_url_template = "https://github.com/%s/releases/download/%s/%s",
+	},
+	pip = {
+		---@since 1.0.0
+		-- Whether to upgrade pip to the latest version in the virtual environment before installing packages.
+		upgrade_pip = false,
+
+		---@since 1.0.0
+		-- These args will be added to `pip install` calls. Note that setting extra args might impact intended behavior
+		-- and is not recommended.
+		--
+		-- Example: { "--proxy", "https://proxyserver" }
+		install_args = {},
+	},
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
+
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls", "gopls" },
+})
+
+-- Set up lspconfig.
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- nvim cmp
+-- Set up nvim-cmp.
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+local lspkind = require("lspkind")
+local cmp = require("cmp")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+				-- they way you will only jump inside the snippet region
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4), -- 向上滚动文档
+		["<C-f>"] = cmp.mapping.scroll_docs(4), -- 向下滚动文档
+		["<C-Space>"] = cmp.mapping.complete(), -- 手动触发补全
+		["<C-e>"] = cmp.mapping.abort(), -- 取消补全
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+		-- 替换方向键为 hjkl
+		["<C-k>"] = cmp.mapping.select_prev_item(), -- 上移补全项
+		["<C-j>"] = cmp.mapping.select_next_item(), -- 下移补全项
+		["<C-h>"] = cmp.mapping.select_prev_item(), -- 左移补全项（示例，可自定义）
+		["<C-l>"] = cmp.mapping.select_next_item(), -- 右移补全项（示例，可自定义）
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = "path" }, -- 路径补全
+		{ name = "buffer" }, -- 缓冲区补全
+	}),
+	window = {
+		-- 提示窗口设置
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(), -- 使用带边框的文档浮窗
+	},
+	formatting = {
+		-- 自定义补全项的显示
+		format = function(entry, vim_item)
+			-- 使用 lspkind 提供的格式化
+			local kind_format = lspkind.cmp_format({
+				mode = "symbol_text", -- 图标 + 文本
+				maxwidth = 50, -- 限制宽度
+				ellipsis_char = "...", -- 超出宽度显示省略号
+			})(entry, vim_item)
+			kind_format.menu = ({
+				buffer = "[Buffer]",
+				nvim_lsp = "[LSP]",
+				luasnip = "[Snippet]",
+				path = "[Path]",
+			})[entry.source.name]
+
+			return kind_format
+		end,
+	},
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
+
+lspconfig.lua_ls.setup({
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT", -- Neovim 使用 LuaJIT 作为运行时
+			},
+			diagnostics = {
+				globals = { "vim", "hs" }, -- 让 LSP 识别 `vim` 全局变量
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true), -- 让 LSP 识别 Neovim 的运行时文件
+			},
+			completion = {
+				callSnippet = "Replace",
+			},
+			telemetry = {
+				enable = false, -- 禁用遥测
+			},
+		},
+	},
+})
+
+-- 配置 gopls
+lspconfig.gopls.setup({
+	capabilities = capabilities,
+	cmd = { "gopls" }, -- 确保 gopls 在系统路径中，或者通过 Mason 安装
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true, -- 开启未使用参数分析
+				nilness = true,
+				shadow = true,
+				unusedwrite = true,
+			},
+			staticcheck = true, -- 启用静态检查
+		},
+	},
+})
